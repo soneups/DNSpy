daccc13e375e4e6e7aaa28267ccffbbbc8985bcc
Gary Sone <gary.sone@bh.sone.net>
2014-12-17 09:08:24 +0000

    Create ns.py

---
+++ ns.py
@@ -0,0 +1,13 @@
+# needs dns module/python installed - HT - http://www.dnspython.org/examples.html
+import dns.resolver
+import sys
+
+if len (sys.argv) != 3 :
+    print 'Usage: please specify <MX|NS> <mx domain name|nameserver>'
+    sys.exit (1)
+
+
+domain = sys.argv[2]
+answers = dns.resolver.query(domain, sys.argv[1])
+for server in answers:
+    print server
